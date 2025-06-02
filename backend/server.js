f5218b6559ac5384dcc5f55dce32f9cae6673f2b
poorna_xiteb <xitebdeveloper18@gmail.com>
2025-06-02 20:07:09 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://myapp-backend.up.railway.app'],
+  origin: ['http://localhost:3000', 'https://usermyapp.netlify.app'],  
   credentials: true
 }));
 
